1622efa7a524f9f3115b92d266533fc18ca5d764
dhanie-ellah <101949297+dhanie-ellah@users.noreply.github.com>
2022-09-30 23:27:29 -0700

    signin/signup form validation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HAMBURGER MENU
 const hamburgerMenu = document.querySelectorAll(".menu")
 const links = document.querySelectorAll(".links")
 const close = document.querySelectorAll(".close")
@@ -30,3 +31,44 @@ close.forEach(closeBtn => {
      })
 })
 
+// SIGNIN/SIGNUP
+const signupForm = document.getElementById('signup-form')
+const signinForm = document.getElementById('signin-form')
+const signupUsername = document.getElementById('signup-username')
+const signinUsername = document.getElementById('signin-username')
+const signupPassword = document.getElementById('signup-password')
+const signinPassword = document.getElementById('signin-password')
+const signupError = document.getElementById('signup-error')
+const signinError = document.getElementById('signin-error')
+let userDatas = []
+
+signupForm.addEventListener('submit', (event) => {
+     event.preventDefault()
+     if (signupUsername.value === '' || signupPassword.value === '') {
+          signupError.innerHTML = 'Field cannot be empty'
+          signupError.style.color = 'red'
+          signupError.style.fontWeight = '700'
+     } else {
+          let userData = {
+               Username: signupUsername.value,
+               Password: signupPassword.value
+          }
+          userDatas.push(userData)
+          window.location = 'signIn.html'
+     }
+})
+
+signinForm.addEventListener('submit', (event) => {
+     event.preventDefault()
+     for (let i = 0; i < userDatas.length; i++) {
+          if (signinUsername.value === '' || signinPassword.value === '') {
+               signinError.innerHTML = 'Field cannot be empty'
+               signinError.style.color = 'red'
+               signinError.style.fontWeight = '700'
+          } else if(signinUsername.value !== userDatas[i].Username || signinPassword.value !== userDatas[i].Password) {
+               signinError.innerHTML = "Username and Password doesn't match"
+          } else if (signinUsername.value === userDatas[i].Username && signinPassword.value === userDatas[i].Password){
+               window.location = 'search.html'
+          }
+     }
+})
